2cea2b54576c11c97219b972fc81abedd55d0a19
Luke Dary <lukedary@gmail.com>
2019-08-22 08:24:51 -0400

    Figuring out UMD

--- src/scripts/@rhd/dp-search/dp-search-app.ts
+++ src/scripts/@rhd/dp-search/dp-search-app.ts
@@ -1,5 +1,5 @@
 //import {PFElement} from '../../@pfelements/pfelement.umd.js';
-import PFElement from '@patternfly/pfelement/pfelement.umd';
+import PFElement from '../@patternfly/pfelement/pfelement.umd.js';
 import DPSearchURL from './dp-search-url';
 import DPSearchModalFilters from './dp-search-modal-filters';
 
--- src/scripts/@rhd/dp-search/dp-search-filter-group.ts
+++ src/scripts/@rhd/dp-search/dp-search-filter-group.ts
@@ -1,7 +1,7 @@
 //import {PFElement} from '../../@pfelements/pfelement.umd.js';
 import { library, icon, dom } from '@fortawesome/fontawesome-svg-core/index'
 import {faChevronRight} from '@fortawesome/pro-solid-svg-icons/index';
-import PFElement from '@patternfly/pfelement/pfelement.umd';
+import PFElement from '../@patternfly/pfelement/pfelement.umd.js';
 import DPSearchFilterItem from './dp-search-filter-item';
 
 library.add(faChevronRight);
